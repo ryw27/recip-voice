41b0d72d684a4717b43945c5100329303983e6b8
ryw27 <rywbook06@gmail.com>
2025-01-02 00:24:15 -0500

    all source files for next

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+}
+
+
+export default nextConfig;
