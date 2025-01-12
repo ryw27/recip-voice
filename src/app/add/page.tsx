bb6c4251dc398f67d4caa9e36e35c07571b79709
ryw27 <rywbook06@gmail.com>
2025-01-11 23:24:04 -0500

    added some input boxes

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -5,8 +5,28 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-around">
       <Input 
-        label="hello"
+        label="Title"
         widthScale={.5} 
+      />
+      <Input 
+        label="Ingredients"
+        widthScale={.5}
+      />
+      <Input 
+        label="Time"
+        widthScale={.5}
+      />
+      <Input 
+        label="Instructions"
+        widthScale={.5}
+      />
+      <Input 
+        label="Tips/Notes"
+        widthScale={.5}
+      />
+      <Input
+        label="Categories"
+        widthScale={.5}
       />
     </main>
   );
