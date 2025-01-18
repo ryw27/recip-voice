c2ac806c15bd9f09281692f923a74562f9b7a835
ryw27 <rywbook06@gmail.com>
2025-01-18 17:35:02 -0500

    changing input boxes to grow height

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,35 +1,52 @@
-"use client"
-import React, {useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 interface InputProps{
   label?: string; 
   placeholder?: string;  
-  widthScale?:number
+  inputText?: string;
+  widthScale?:number;
 }
 
-const Input = ({label, placeholder="enter text", widthScale=1}: InputProps) => {
+const Input = ({label, placeholder="Enter Text", inputText, widthScale=1}: InputProps) => {
     const [inputValue, setInputValue] = useState("");
+    const inputRef = useRef<HTMLTextAreaElement>(null);
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputValue(e.target.value);
+        if (inputRef.current) {
+            console.log("hello");
+            inputRef.current.style.height = "auto";
+            inputRef.current.style.height = `${inputRef.current.scrollHeight}px`
+        } 
     }
 
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.style.height = "auto";
+            inputRef.current.style.height = `${inputRef.current.scrollHeight}px`
+        }
+        if (inputText) {
+            setInputValue(inputText);
+        }
+        console.log(inputValue);
+    }, [inputText])
 
     const calculatedWidth = `${widthScale * 100}vw`;
 
     return (
         <div style={{ width : calculatedWidth }} className="">
             {label && (
-                <label className="block text-gray-700 text-sm font-medium m-2">
+                <label className="block text-sm font-medium m-2">
                     {label}
                 </label>
             )}
-            <input 
-                type="text"
+            <textarea
+                ref={inputRef}
                 value={inputValue}
                 onChange={handleInputChange}
                 placeholder={placeholder}
-                className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                className="w-full px-4 py-2 border border-gray-300 text-black rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                rows={1}
             />
         </div>
         
