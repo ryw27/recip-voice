5bafbb6bcbc0a121ba9e4667b0c93db87b1d56a1
ryw27 <rywbook06@gmail.com>
2025-01-18 17:36:51 -0500

    alert when no transcript/voice recorded

--- src/components/VoiceRecorder.tsx
+++ src/components/VoiceRecorder.tsx
@@ -76,15 +76,19 @@ export default function VoiceRecorder() {
       //             into a coherent recipe with a title, ingredients, estimated time, and instructions`,
       // });
       // console.log(textStream);
-
-      router.push("add")
-    }
+      if (transcript) {
+        localStorage.setItem('transcript', JSON.stringify({transcript}));
+        router.push('/create');
+      } else {
+        alert("No audio recognized");
+      }
+   }
   }
 
   return (
-    <div className="flex w-3/4 h-2/5 justify-center items-center rounded-lg flex-col"> 
+    <div className="flex w-3/4 h-2/5 justify-center items-center flex-col"> 
       {(isRecording || transcript) &&
-        <div className="w-1/2 bg-white flex flex-col rounded-lg p-12 mb-[24] text-center">
+        <div className="w-1/2 bg-white text-black flex flex-col rounded-lg p-12 mb-[24] text-center">
           <p className="font-bold">
             {recordingComplete ? "Recorded" : "Recording"}
           </p>
@@ -94,13 +98,13 @@ export default function VoiceRecorder() {
         </div>
       }
       <button onClick={isRecording ? stopRecording : startRecording} 
-              className={`rounded-full w-10 h-10 bg-red-400 flex items-center justify-center ${isRecording ? 'animate-pulse' : ''}`}>
-        <svg className="w-8 h-8" data-slot="icon" aria-hidden="true" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              className={`rounded-full w-[50px] h-[50px] bg-red-400 flex items-center justify-center ${isRecording ? 'animate-pulse' : ''}`}>
+        <svg className="w-10 h-10" data-slot="icon" aria-hidden="true" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" strokeLinecap="round" strokeLinejoin="round"></path>
         </svg>
       </button>
 
-      {true && (
+      {transcript && (
         <div className="text-center">
           <h3 className="font-bold">Transcript:</h3>
           <p>{transcript}</p>
