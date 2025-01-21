1d87ae069afeb6c49cd7cdd6432cbf468f439dcd
ryw27 <rywbook06@gmail.com>
2025-01-20 23:55:09 -0500

    supabase integration with initalization and cookies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import VoiceRecorder from '@/components/VoiceRecorder';
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from('todos').select()
   return (
     // <main className="flex h-screen items-center w-screen">
       /* <div className="flex flex-col h-full">
@@ -10,7 +16,11 @@ export default function Home() {
       <div className="flex justify-around flex-col items-center h-full w-full">
         {/* <h1 className="text-2xl font-bold">Save</h1> */}
         <VoiceRecorder />
-        
+        <ul>
+          {todos?.map((todo:any) => (
+          <li>{todo}</li>
+          ))}
+        </ul>
       </div>
     // </main>
   );
