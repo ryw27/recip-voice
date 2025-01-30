29d3fe99ad56a3e655e0341f70faeecc05c1c013
ryw27 <rywbook06@gmail.com>
2025-01-29 23:58:56 -0500

    more supabase integration attempts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 if (!supabaseURL || !supabaseAnonKey || !supabaseServiceKey) {
     throw new Error("Missing supabase keys");
